c455e98c7e3c9d1d7fb9c0611840ad1831d1983f
Cem Sancak <cem.sancak.90@gmail.com>
2020-01-07 15:16:28 +0300

    initial

---
+++ main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+func main() {
+
+	files := os.Args[1:]
+
+	baseFile, others := files[0], files[1:]
+
+	b, err := ioutil.ReadFile(baseFile)
+	if err != nil {
+		panic(err)
+	}
+
+	var base map[interface{}]interface{}
+	if err = yaml.Unmarshal(b, &base); err != nil {
+		panic(err)
+	}
+	stringBase := turnToString(base)
+
+	for _, file := range others {
+		b, err = ioutil.ReadFile(file)
+		if err != nil {
+			panic(err)
+		}
+
+		var patch map[interface{}]interface{}
+		if err = yaml.Unmarshal(b, &patch); err != nil {
+			panic(err)
+		}
+		stringPatch := turnToString(patch)
+
+		applyPatch(stringBase, stringPatch)
+	}
+
+	b, err = yaml.Marshal(stringBase)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(b))
+}
+
+func turnToString(patch map[interface{}]interface{}) map[string]interface{} {
+	m := make(map[string]interface{})
+	for k, v := range patch {
+		switch vt := v.(type) {
+		case []interface{}:
+			for i, vv := range vt {
+				switch vvt := vv.(type) {
+				case map[interface{}]interface{}:
+					vt[i] = turnToString(vvt)
+				default:
+					vt[i] = vvt
+				}
+			}
+			m[k.(string)] = vt
+		case map[interface{}]interface{}:
+			m[k.(string)] = turnToString(vt)
+		default:
+			m[k.(string)] = vt
+		}
+	}
+	return m
+}
+
+func applyPatch(base, patch map[string]interface{}) {
+	for k, v := range patch {
+		switch vt := v.(type) {
+		case []interface{}:
+			vv, ok := base[k].([]interface{})
+			if !ok {
+				vv = make([]interface{}, len(vt))
+			}
+			for i, vvv := range vt {
+				switch vvvt := vvv.(type) {
+				case map[string]interface{}:
+					var vvvv map[string]interface{}
+					if len(vv) <= i {
+						vvvv = make(map[string]interface{})
+						vv = append(vv, vvvv)
+					} else {
+						vvvv, ok = vv[i].(map[string]interface{})
+						if !ok {
+							vvvv = make(map[string]interface{})
+							vv[i] = vvvv
+						}
+					}
+					applyPatch(vvvv, vvvt)
+				default:
+					base[k] = vvvt
+				}
+			}
+			base[k] = vv
+		case map[string]interface{}:
+			vv, ok := base[k].(map[string]interface{})
+			if !ok {
+				vv = make(map[string]interface{})
+				base[k] = vv
+			}
+			applyPatch(vv, vt)
+		default:
+			base[k] = vt
+		}
+	}
+}
